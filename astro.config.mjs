b59525bdbec2e9c6bf2a120e273cda861f7d56da
TYD5630 <133104885+TYD5630@users.noreply.github.com>
2026-03-12 16:31:25 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind';  // Add this line
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
